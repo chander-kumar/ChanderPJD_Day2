a0fa1416b3b41bb73432078e133caa32614c981b
ZainulAbidin-dot <zchxvr@gmail.com>
2022-06-21 23:48:34 +0500

    1

---
+++ Zain/Day1/main.js
@@ -0,0 +1,11 @@
+// Euler's formula
+var e = math.e;
+var pi = math.pi;
+var i = math.i.im;
+
+var eulerIdentity = eval(e^(pi*i) + 1);
+// console.log(e);
+// console.log(pi);
+// console.log(i);
+console.log('Error due to numerical round off errors and has been fixed in python')
+console.log(eulerIdentity);
