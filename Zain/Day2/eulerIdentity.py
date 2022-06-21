3313ddea8294aaa7a6429fe7f3fec0848351114b
ZainulAbidin-dot <zchxvr@gmail.com>
2022-06-21 19:30:39 +0500

    first

--- Zain/Day2/eulerIdentity.py
+++ Zain/Day2/eulerIdentity.py
@@ -1,7 +1,10 @@
 # Supported by: M.Zain ul Abidin
 from sympy import *
+import streamlit as st
 
 def my_function():
-  print(E**(pi*I) + 1)
-
-my_function()
+  return(E**(pi*I) + 1)
+  try:
+    my_function()
+  except:
+    st.write("Error")
